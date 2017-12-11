691a2ebc382c58eed6186c37a740074eae584a1b
Jacek Królikowski <nietaki@gmail.com>
2017-12-11 18:13:05 +0000

    cleanup. script to run swarm. copying everything at once

--- packer/build_swarm_binaries.sh
+++ packer/build_swarm_binaries.sh
@@ -2,12 +2,6 @@
 
 # taken from https://github.com/MainframeHQ/onyx-server/blob/master/start_swarm_node.sh
 
-if [[ $# -ne 1 ]]; then
-  echo "Usage: $0 <data-directory>"
-  exit 1
-fi
-
-DATADIR="$1"
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 EXT_DEPS_DIR="$SCRIPTPATH/external_deps"
 GODIR="$EXT_DEPS_DIR/go-ethereum"
@@ -30,30 +24,3 @@ make swarm
 
 cp build/bin/geth /home/ubuntu/geth_binaries
 cp build/bin/swarm /home/ubuntu/geth_binaries
-
-# if [[ ! -e $DATADIR/keystore ]]; then
-#   mkdir -p $DATADIR
-#   echo 'secret' > $DATADIR/password
-#   $GODIR/build/bin/geth --datadir $DATADIR account new --password $DATADIR/password
-# fi
-
-# which jq
-# if [ $? -eq 0 ]
-# then
-#     KEY=$(jq --raw-output '.address' $DATADIR/keystore/*)
-# else
-#     printf "\n\nERROR: jq is required to run the startup script\n\n"
-#     exit 1
-# fi
-
-# $GODIR/build/bin/swarm \
-#     --datadir $DATADIR \
-#     --password $DATADIR/password \
-#     --verbosity 4 \
-#     --bzzaccount $KEY \
-#     --pss \
-#     --bzznetworkid 922 \
-#     --bootnodes enode://e834e83b4ed693b98d1a31d47b54f75043734c6c77d81137830e657e8b005a8f13b4833efddbd534f2c06636574d1305773648f1f39dd16c5145d18402c6bca3@54.171.164.15:30399 \
-#     --ws \
-#     --wsorigins '*'
-
--- packer/example.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "I'm an example script!"
