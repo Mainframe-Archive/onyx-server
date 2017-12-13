129a0a75ae6443ee44cf95d3b9ca506d3b105f69
Shane Howley <shane@mainframe.com>
2017-12-13 20:36:19 +0000

    Add certificate generation on first run

--- packer/run-onyx-server.sh
+++ packer/run-onyx-server.sh
@@ -2,9 +2,19 @@
 set -x
 
 SLEEP_TIME=20
+HOME_DIR="/home/ubuntu"
+CERTS_DIR="$HOME_DIR/certs"
 
 echo "giving swarm $SLEEP_TIME seconds to start up"
 sleep "$SLEEP_TIME"
 
+if [[ ! -e $CERTS_DIR ]]; then
+  ONYX_DIR="$(npm root -g)/onyx-server"
+
+  pushd "$HOME_DIR"
+  "$ONYX_DIR/scripts/gen-certs.sh"
+  popd
+fi
+
 echo "starting onyx-server"
-onyx-server --port 5000 --http-url http://localhost:8500 --ws-url ws://localhost:8546 --unsecure
+onyx-server --port 5000 --http-url https://onyx-storage.mainframe.com --ws-url ws://localhost:8546 --certs-dir "$CERTS_DIR"
