1fbde8dbc1f8c7b8bdd3e1c28d2c686450f694cc
Jacek Królikowski <nietaki@gmail.com>
2017-12-14 12:05:40 +0000

    detecting public ip/hostname on aws

--- packer/run-onyx-server.sh
+++ packer/run-onyx-server.sh
@@ -4,6 +4,9 @@ set -x
 SLEEP_TIME=20
 HOME_DIR="/home/ubuntu"
 CERTS_DIR="$HOME_DIR/certs"
+# these should come out empty if the host doesn't have a public ip/hostname
+PUBLIC_IP=$(curl -f --silent http://169.254.169.254/latest/meta-data/public-ipv4)
+PUBLIC_HOSTNAME=$(curl -f --silent http://169.254.169.254/latest/meta-data/public-hostname)
 
 echo "giving swarm $SLEEP_TIME seconds to start up"
 sleep "$SLEEP_TIME"
